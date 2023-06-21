24a2f71d1e1190e018e6d99b61f80e63fb312b40
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-21 18:51:44 -0500

    schema.sql complete

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+)
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    foreign key (department_id) references department(id)
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    foreign key (role_id) references role(id),
+    foreign key (manager_id) references employee(id)
+)
